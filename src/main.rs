f6bf19faa452f363ebdae5f3cdd694d90ce10cc3
Keanu Poeschko <nur1popcorn@gmail.com>
2022-12-13 13:45:05 +0100

    Add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+use clap::Parser;
+
+/// A simple program to judge your usage of git
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Sets a specific author
+    #[arg(short, long)]
+    author: Option<String>,
+
+    /// Sets a cutoff date
+    #[arg(short, long)]
+    date: Option<u8>,
+}
+
 fn main() {
+    let args = Args::parse();
 }
