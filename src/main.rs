138e31c83c224ed43ff9e829e3598b128bd0f59b
Keanu Poeschko <nur1popcorn@gmail.com>
2022-12-14 14:04:39 +0100

    Add basic score printer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod stats;
 
+use std::cmp::min;
+use std::collections::HashMap;
 use clap::Parser;
 use git2::Repository;
 
@@ -13,9 +15,16 @@ struct Args {
     #[arg(short, long)]
     author: Option<String>,
 
+
     /// The path to the repository which should be scored
     #[arg(group = "input", default_value_t = String::from("."))]
-    input_path: String
+    input_path: String,
+
+    /// Indicates if the used issuer key id is unique false by default
+    #[arg(long, default_value_t = false)]
+    issuer: bool
+    // detail flag provides graphs for author
+    // list good commits i.e. commits with a high score
 }
 
 fn main() {
@@ -33,10 +42,26 @@ fn main() {
         let commit = repo.find_commit(oid).unwrap();
         let signature = repo.extract_signature(&oid, None);
         let stats = Stats::from(&repo, &commit, signature.is_ok());
-        let author = Author::new(commit.author(), signature.ok());
+        let author = Author::new(commit.author(),
+            if args.issuer { signature.ok() } else { None });
         result_vec.push((author, stats));
     }
 
-    result_vec.sort_by(|a, b| { a.1.score().cmp(&b.1.score()) });
-    println!("{:?}", result_vec);
+    let mut result_map = HashMap::new();
+    for (author, stats) in result_vec {
+        *result_map.entry(author).or_insert(0) += stats.score();
+    }
+
+    let mut result_vec = Vec::new();
+    for p in result_map {
+        result_vec.push(p)
+    }
+    result_vec.sort_by(|a, b| { b.1.cmp(&a.1) });
+    for i in 0 .. min(result_vec.len(), 10) {
+        let author = &result_vec[i].0;
+        let name = &author.name;
+        let email = &author.email;
+        let score = &result_vec[i].1;
+        println!("#{} {name} {email} {score}", i + 1);
+    }
 }
